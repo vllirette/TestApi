565d4601690027a0a254e519e071857d33dc5207
vincentLirette <vi.lirette@gmail.com>
2018-11-24 16:33:40 -0500

    Add Swagger UI

--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 using TodoApi.Models;
 using TodoApi.Repository;
 
@@ -16,7 +20,7 @@ namespace TodoApi
         }
 
         public IConfiguration Configuration { get; }
-        
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<TodoContext>(opt =>
@@ -24,17 +28,34 @@ namespace TodoApi
             services.AddMvc()
                     .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.Configure<Settings>(options => {
-                        options.ConnectionString = Configuration.GetSection("MongoDb:ConnectionString").Value;
-                        options.Database = Configuration.GetSection("MongoDb:Database").Value;
-                        });
-            
+            services.Configure<Settings>(options =>
+            {
+                options.ConnectionString = Configuration.GetSection("MongoDb:ConnectionString").Value;
+                options.Database = Configuration.GetSection("MongoDb:Database").Value;
+            });
+
             services.AddTransient<IGameContext, GameContext>();
             services.AddTransient<IGameRepository, GameRepository>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "TestAPI", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestAPI v1");
+                c.RoutePrefix = string.Empty;
+
+            });
             app.UseMvc();
         }
     }
